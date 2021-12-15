29da9bb2cad12f6eb46d773c516b689f27dbba84
Tkalex32 <tkalexsc@gmail.com>
2021-12-15 17:45:09 +0100

    finalize functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,64 @@
+import { useState } from "react";
+
 const api = {
   key: import.meta.env.VITE_API_KEY,
   base: "https://api.openweathermap.org/data/2.5/",
 };
 
+const options = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+};
+const today = new Date().toLocaleString("hu-HU", options);
+
 function App() {
-  const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = (e) => {
+    if (e.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&appid=${api.key}`)
+        .then((res) => res.json())
+        .then((res) => {
+          setWeather(res);
+          setQuery("");
+          console.log(weather.weather);
+        });
+    }
   };
-  const today = new Date().toLocaleString("hu-HU", options);
 
   return (
-    <div className="app">
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 16
+            ? "app warm"
+            : "app"
+          : "app"
+      }
+    >
       <main>
         <div className="search-box">
-          <input type="text" className="search-bar" placeholder="Search..." />
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
         </div>
         <div className="location-box">
-          <div className="location">New York, US</div>
+          <div className="location">
+            {weather.name}, {weather.sys.country}
+          </div>
           <div className="date">{today}</div>
         </div>
         <div className="weather-box">
-          <div className="temp">15&deg;C</div>
-          <div className="weather">Sunny</div>
+          <div className="temp">{Math.round(weather.main.temp)}&deg;C</div>
+          <div className="weather">{weather.weather[0].main}</div>
         </div>
       </main>
     </div>
